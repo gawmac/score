11ce9e38147d1e1d04e6017543489c13a367c591
gawmac <maciej.gawlik@gmail.com>
2018-05-08 16:14:16 +0200

    Added handlebars

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 
 const app = express();
 
+//Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 //Index Route
 app.get('/', (req, res) => {
-    res.send('INDEX');
+    res.render('index');
 });
 
 //About
 app.get('/about', (req, res) => {
-    res.send('ABOUT');
+    res.render('about');
 });
 
 const port = 5000;
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
